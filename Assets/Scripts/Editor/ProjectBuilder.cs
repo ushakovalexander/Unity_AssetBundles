37b0c1916289b8396876939cb7ab976a25942e50
Alexander Ushakov <ushakov@renatus.com>
2018-01-18 20:43:58 +0200

    Implement asset bundles and game builder for Win32.

---
+++ Assets/Scripts/Editor/ProjectBuilder.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System;
+using System.Diagnostics;
+
+public class ProjectBuilder  {
+  [MenuItem("Project Builder/Build Win32")]
+  public static void BuildProjectWin32() {
+    Build(BuildTarget.StandaloneWindows);
+  }
+
+  private static void Build(BuildTarget target) {
+    BuildAssetBundles(target);
+    BuildGame(target);
+  }
+
+  private static void BuildGame(BuildTarget target) {
+    string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+    string[] levels = new string[] {"Assets/Main.unity"};
+
+    BuildPipeline.BuildPlayer(levels, path + "/Game.exe", target, BuildOptions.None);
+
+    Process proc = new Process();
+    proc.StartInfo.FileName = path + "/Game.exe";
+    proc.Start();
+  }
+
+  private static void BuildAssetBundles(BuildTarget target) {
+    var bundleNames = AssetDatabase.GetAllAssetBundleNames();
+    foreach (var name in bundleNames) {
+      UnityEngine.Debug.Log ("AssetBundle: " + name);
+    }
+    BuildPipeline.BuildAssetBundles(CheckBundleTargetFolder(GetBundleTargetFolderPath()), BuildAssetBundleOptions.None, target);
+  }
+
+  private static string GetBundleTargetFolderPath() {
+    return Application.dataPath + "/StreamingAssets";
+  }
+
+  private static string CheckBundleTargetFolder(string path) {
+     if(!Directory.Exists(path)) {
+       Directory.CreateDirectory(path);
+     }
+     return path;
+  }
+}
---
+++ Assets/Scripts/InitPlatforms.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class InitPlatforms : MonoBehaviour {
+  [SerializeField]
+  private int _maxPlatforms = 100;
+  private List<GameObject> _platforms;
+  private Vector3 _nextPosition;
+  private int _counter;
+
+  void Start () {
+    var prototypes = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "prototyping"));
+    if (prototypes == null) {
+      Debug.Log("Failed to load AssetBundle!");
+      return;
+    }
+
+    _platforms = new List<GameObject>();
+    _platforms.Add(prototypes.LoadAsset("PlatformPrototype02x01x02") as GameObject);
+    _platforms.Add(prototypes.LoadAsset("PlatformPrototype04x01x04") as GameObject);
+    _platforms.Add(prototypes.LoadAsset("PlatformPrototype08x01x08") as GameObject);
+  }
+
+  void Update () {
+    if(_counter > _maxPlatforms) {
+      enabled = false;
+    }
+    CreatePlatform(GetRandomPlatform());
+  }
+
+  private GameObject GetRandomPlatform() {
+    if(_platforms != null && _platforms.Count > 0) {
+      int index = UnityEngine.Random.Range(0, _platforms.Count);
+      return _platforms[index];
+    }
+    return null;
+  }
+
+  private void CreatePlatform(GameObject platform) {
+    if(platform != null) {
+      Instantiate(platform, _nextPosition, Quaternion.identity);
+      _counter++;
+      _nextPosition.Set(_nextPosition.x + platform.GetComponent<Renderer>().bounds.size.x, _nextPosition.y, _nextPosition.z);
+    }
+  }
+}
