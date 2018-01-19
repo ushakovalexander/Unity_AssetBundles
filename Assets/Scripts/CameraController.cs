b587b5af926e4a2bedba631d7b689fd78a79c7ab
Alexander Ushakov <ushakov@renatus.com>
2018-01-19 19:57:45 +0200

    Update Main scene: platform builder, camera. Update materials. Cleanup.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+  private GameObject _cameraTarget;
+  private Vector3 _offset;
+  private Vector3 _velocity = Vector3.zero;
+  [SerializeField]
+  private float smoothTime = 0.1f;
+
+  public GameObject CameraTarget {
+    get {
+      return _cameraTarget;
+    }
+    set {
+      _cameraTarget = value;
+    }
+  }
+
+  void Update() {
+    if(_offset == Vector3.zero) {
+      _offset = transform.position;
+    }
+    if(CameraTarget != null) {
+      transform.position = Vector3.SmoothDamp(transform.position, CameraTarget.transform.position + _offset, ref _velocity, smoothTime);
+    }
+  }
+}
--- Assets/Scripts/PlatformsBuilder.cs
+++ Assets/Scripts/PlatformsBuilder.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 
-public class InitPlatforms : MonoBehaviour {
+public class PlatformsBuilder : MonoBehaviour {
   [SerializeField]
-  private int _maxPlatforms = 100;
+  private CameraController _camera;
+  [SerializeField]
+  private float _waitToCreateSec;
   private List<GameObject> _platforms;
   private Vector3 _nextPosition;
-  private int _counter;
+  private WaitForSeconds _waitToCreate;
+  private WaitForSeconds _waitToDestroy;
 
   void Start () {
     var prototypes = AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "prototyping"));
@@ -22,13 +25,17 @@ public class InitPlatforms : MonoBehaviour {
     _platforms.Add(prototypes.LoadAsset("PlatformPrototype02x01x02") as GameObject);
     _platforms.Add(prototypes.LoadAsset("PlatformPrototype04x01x04") as GameObject);
     _platforms.Add(prototypes.LoadAsset("PlatformPrototype08x01x08") as GameObject);
+
+    _waitToCreate = new WaitForSeconds(_waitToCreateSec);
+    _waitToDestroy = new WaitForSeconds(_waitToCreateSec * 40);
+    StartCoroutine(SpawnPlatforms());
   }
 
-  void Update () {
-    if(_counter > _maxPlatforms) {
-      enabled = false;
+  private IEnumerator SpawnPlatforms() {
+    while(true) {
+      CreatePlatform(GetRandomPlatform());
+      yield return _waitToCreate;
     }
-    CreatePlatform(GetRandomPlatform());
   }
 
   private GameObject GetRandomPlatform() {
@@ -41,9 +48,20 @@ public class InitPlatforms : MonoBehaviour {
 
   private void CreatePlatform(GameObject platform) {
     if(platform != null) {
-      Instantiate(platform, _nextPosition, Quaternion.identity);
-      _counter++;
-      _nextPosition.Set(_nextPosition.x + platform.GetComponent<Renderer>().bounds.size.x, _nextPosition.y, _nextPosition.z);
+      GameObject newPlatform = Instantiate(platform, _nextPosition, Quaternion.identity);
+      StartCoroutine(RemovePlatform(newPlatform));
+      _nextPosition.Set(_nextPosition.x + 1, _nextPosition.y + platform.GetComponent<Renderer>().bounds.size.y, _nextPosition.z);
+      if(_camera != null) {
+        _camera.CameraTarget = newPlatform;
+      }
     }
+  }
+
+  private IEnumerator RemovePlatform(GameObject platform) {
+    if(platform == null) {
+      yield return null;
+    }
+    yield return _waitToDestroy;
+    Destroy(platform);
   }
 }
